626ba607d02452942463efbf31df3b2d69ba9f38
Dan <lyash.dv@gmail.com>
2021-01-24 11:02:02 +0200

    init

---
+++ src/test/java/com/currencybot/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencybot.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
