890e9f090e43eed74e5f33b7cac5d76bbac0f3bc
Dan <lyash.dv@gmail.com>
2021-01-26 04:46:23 +0200

    unit tests

--- src/test/java/com/currencybot/services/impl/UserServiceImplTest.java
+++ src/test/java/com/currencybot/services/impl/UserServiceImplTest.java
@@ -9,13 +9,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
+@PropertySource("classpath:application.properties")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest
 class UserServiceImplTest {
     @Autowired
@@ -46,6 +56,24 @@ class UserServiceImplTest {
                 .save(user);
 
         Mockito.doReturn(Optional.empty())
+                .when(userRepository)
+                .findById(1);
+
+
+        User savedUser = userService.saveOrGetUser(user);
+
+        Assert.assertEquals(user.getId(), savedUser.getId());
+    }
+
+    @Test
+    public void saveOrGetGetTest() {
+        User user = User.builder().id(1).name("name").botState(BotState.START).build();
+
+        Mockito.doReturn(user)
+                .when(userRepository)
+                .save(user);
+
+        Mockito.doReturn(Optional.of(user))
                 .when(userRepository)
                 .findById(1);
 
---
+++ src/test/java/com/currencybot/utils/MessageUtilsTest.java
@@ -0,0 +1,81 @@
+package com.currencybot.utils;
+
+import com.currencybot.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@TestPropertySource(locations = "classpath:test.properties")
+@SpringBootTest
+class MessageUtilsTest {
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Test
+    public void formMessageTest() {
+        String chatId = "1";
+        String text = "text";
+
+        List<InlineKeyboardButton> inlineKeyboardButtons =
+                List.of(
+                        InlineKeyboardButton
+                                .builder()
+                                .text("")
+                                .callbackData("")
+                                .build()
+                );
+        InlineKeyboardMarkup inlineKeyboardMarkup =
+                InlineKeyboardMarkup.builder().keyboard(List.of(inlineKeyboardButtons)).build();
+
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(chatId)
+                .text(text)
+                .replyMarkup(inlineKeyboardMarkup)
+                .build();
+
+        SendMessage generatedMessage = MessageUtils.formMessage(chatId, text, List.of(inlineKeyboardButtons));
+
+        Assert.assertEquals(sendMessage, generatedMessage);
+    }
+
+    @Test
+    public void formKeyboardRowTest() {
+        String text = "text";
+        String callbackData = "callbackData";
+
+        List<InlineKeyboardButton> inlineKeyboardButtons =
+                List.of(
+                        InlineKeyboardButton
+                                .builder()
+                                .text(text)
+                                .callbackData(callbackData)
+                                .build()
+                );
+
+        List<InlineKeyboardButton> formedInlineKeyboardButtons =
+                MessageUtils.formKeyboardRow(List.of(
+                        new ButtonSpecs(text,callbackData)
+                ));
+
+        Assert.assertEquals(inlineKeyboardButtons,formedInlineKeyboardButtons);
+    }
+}
